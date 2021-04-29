e8f02325c4c31edf969cea21352151629a3c3a59
Vijayabhaarathy T <vijayabhaarathy@gmail.com>
2021-04-29 08:10:15 +0000

    Add new file

---
+++ CrossProduct.py
@@ -0,0 +1,9 @@
+#3. Write a NumPy program to compute the cross product of two given vectors.
+
+import numpy as np
+arr1=np.array([1,2,3])
+arr2=np.array([4,5,6])
+
+arr_cp=np.cross(arr1,arr2)
+
+print(arr_cp)
