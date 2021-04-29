53646238bcd8437a8074905726c54abb342d7aa3
Vijayabhaarathy T <vijayabhaarathy@gmail.com>
2021-04-29 08:09:29 +0000

    Add new file

---
+++ SetDiffference.py
@@ -0,0 +1,6 @@
+#2. Write a NumPy program to find the set difference of two arrays. The set difference will return the sorted, unique values in array1 that are not in array2.
+
+set1={1,2,3,4,5,6,7,8,9,10}
+set2={10,8,6,4,2}
+
+set1-set2
